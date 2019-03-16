7c295cdb891b5b11ee54bb425716a6455718c7b6
Hieu <hieumaster17@gmail.com>
2019-03-17 02:41:42 +0700

    Add validate

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -15,21 +15,6 @@ module.exports.create = (req, res) => {
     res.render('users/create')
 }
 module.exports.postCreate = (req, res) => {
-    var errors = [];
-
-    if (!req.body.name) {
-        errors.push('Name is required');
-    }
-    if (!req.body.phone) {
-        errors.push('Phone is required');
-    }
-    if (errors.length) {
-        res.render('users/create', {
-            errors: errors,
-            values: req.body,
-        })
-        return
-    }
     db.get('users').push(req.body).write()
     res.redirect('/users')
 };
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var controller = require('../controllers/user.controller')
 var router = express.Router();
-
+var validate = require('../validate/user.validate')
 router.get('/', controller.index)
 router.get('/search', controller.search)
 router.get('/create', controller.create)
-router.post('/create', controller.postCreate)
+router.post('/create',validate.postCreate, controller.postCreate)
 
 module.exports = router;
